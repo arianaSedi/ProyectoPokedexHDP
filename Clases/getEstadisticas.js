6e439aceb1e827ea5ec166504b1c6421720f9433
Angélica Pérez <ap22010@ues.edu.sv>
2025-06-11 19:12:25 -0600

    Create getEstadisticas.js
    creación de la función de stats

---
+++ Clases/getEstadisticas.js
@@ -0,0 +1,35 @@
+import Estadisticas from "./Estadisticas.js";
+
+const getEstadisticas = (dtPoke) =>{
+
+    const data = dtPoke;
+    const stats = new Estadisticas();
+
+    for (let i = 0; i < data.length; i++) {
+        
+        if(i == 0){
+            stats.setHP(data[i].base_stat);
+        }
+        else if (i == 1){
+            stats.setAttack(data[i].base_stat);
+        }
+        else if (i == 2){
+            stats.setDefense(data[i].base_stat);
+        }
+        else if (i == 3){
+            stats.setSpecialAttack(data[i].base_stat);
+        }
+        else if (i == 4){
+            stats.setSpecialDefense(data[i].base_stat);
+        }
+        else {
+            stats.setSpeed(data[i].base_stat);
+        }
+        
+    }
+
+    return stats;
+
+}
+
+export default getEstadisticas;
